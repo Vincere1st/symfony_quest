06d0975e098f604e7ef44c47b4a7d917d1dbbc47
Vincere1st <vincerecaro@gmail.com>
2019-06-17 10:30:36 +0200

    adduser

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -44,10 +44,11 @@ class Article
      */
     private $slug;
 
-    public function __construct()
-    {
-        $this->tags = new ArrayCollection();
-    }
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="articles")
+     */
+    private $author;
+
 
     public function getId(): ?int
     {
@@ -126,6 +127,18 @@ class Article
     public function setSlug(string $slug): self
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
